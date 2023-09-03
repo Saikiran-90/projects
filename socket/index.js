10f451d6c1122a0f55d447a931bd84ae110bdd09
SAI KIRAN <saikiran6695@gmail.com>
2023-09-03 22:31:09 +0530

    Connected MongoDb and created DB chatapp.

--- socket/index.js
+++ socket/index.js
@@ -4,31 +4,53 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import http from 'http';
 import { Server } from 'socket.io';
+import  mongoose  from 'mongoose';
 
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 9009;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+// connect to mongodb url
+ mongoose.connect("mongodb://localhost:27017/chatapp",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+console.log("Connected to mongodb");
+
+const Schema1 = new mongoose.Schema({
+    name1: String,
+});
+
+const Collection = mongoose.model("User",Schema1);
+const message = mongoose.model("Message",Schema1);
 
 app.get("/",(req,resp)=>{
     resp.sendFile(__dirname + "/public/home.html");
-})
+});
 
 app.post("/chat",(req,resp)=>{
-    const name = req.body["name1"];
-    console.log(name);
-    resp.sendFile(__dirname+"/public/index.html",name);
+    const nm = req.body["name1"];
+    const User = new Collection({name1:nm});
+    User.save();
+  
+    console.log(User);
+    resp.sendFile(__dirname+"/public/index.html");
 });
 
 io.on('connection',(sock)=>{
   
     sock.on('message'  ,(msg)=>{
+        const Message = new message({name1:msg});
+        Message.save();   
     console.log(msg);
        io.emit('message',msg);
  });
@@ -37,5 +59,3 @@ io.on('connection',(sock)=>{
 server.listen(port,()=>{
     console.log(`Server started At Location ${port}`);
 });
-
-
