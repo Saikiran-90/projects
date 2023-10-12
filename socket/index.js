ea0cbd29e3b29431768ebac7621342c50479f040
SAI KIRAN <saikiran6695@gmail.com>
2023-10-12 13:58:04 +0530

    Added names and Modified home.html

--- socket/index.js
+++ socket/index.js
@@ -1,11 +1,10 @@
 import express from 'express';
+// import session from 'express-session';
 import bodyParser from 'body-parser';
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import http from 'http';
 import { Server } from 'socket.io';
-import  mongoose  from 'mongoose';
-
 
 const app = express();
 const port = 9009;
@@ -16,44 +15,37 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const server = http.createServer(app);
 const io = new Server(server);
 
+
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended:true}));
 
-// connect to mongodb url
- mongoose.connect("mongodb://localhost:27017/chatapp",{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-console.log("Connected to mongodb");
-
-const Schema1 = new mongoose.Schema({
-    name1: String,
-});
-
-const Collection = mongoose.model("User",Schema1);
-const message = mongoose.model("Message",Schema1);
 
 app.get("/",(req,resp)=>{
     resp.sendFile(__dirname + "/public/home.html");
 });
 
-app.post("/chat",(req,resp)=>{
-    const nm = req.body["name1"];
-    const User = new Collection({name1:nm});
-    User.save();
-  
-    console.log(User);
-    resp.sendFile(__dirname+"/public/index.html");
-});
+
+app.get("/chat",(req,resp)=>{
+      resp.sendFile(__dirname+"/public/index.html");
+});  
+
+const users = [];
+
 
 io.on('connection',(sock)=>{
-  
-    sock.on('message'  ,(msg)=>{
-        const Message = new message({name1:msg});
-        Message.save();   
-    console.log(msg);
-       io.emit('message',msg);
- });
+   
+     sock.on('new-user-joined', (name)=>{
+          console.log("New user: ",name);
+          users[sock.id] = name;
+          console.log(users);
+          io.emit('user-joined',name);
+     });
+
+
+    sock.on('message',(msg)=>{
+        console.log(msg);
+        io.emit('message',{'message': msg , 'name' : users[sock.id]});
+   });
 });
 
 server.listen(port,()=>{
